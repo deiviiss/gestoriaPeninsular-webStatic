a77d79cfde9be3f829fbef2d05837e8a2b8f1b7e
David <david.hilera@hotmail.com>
2020-11-01 14:25:10 -0600

    Funcionalidad completa

--- js/scripts.js
+++ js/scripts.js
@@ -3,22 +3,62 @@ document.querySelector('.header__logo--btnDesktop').addEventListener('click', ()
   document.querySelector('.nav').classList.toggle('show')
 })
 
+let cantidadARetirar = document.getElementById("cantidadRetiro");
+let btnCalcular = document.getElementById("btnCalcular")
 
-var valor = document.getElementById("cantidadRetiro");
-var boton = document.getElementById("boton")
-var cantidad1;
-
-boton.addEventListener("click", calcularRetiro)
+btnCalcular.addEventListener("click", calcularRetiro)
 
 function calcularRetiro() {
-  var cantidad1 = valor.value;
+  let aseguramiento;
+  let asesoria;
+
+  let cantidad = cantidadARetirar.value;
 
 
+  if (cantidad > 30999) {
 
+    let cobro = cantidad * .25;
+    alert("Asesoría: $" + cobro + "\n" + "Aseguramiento: Incluido\n" + "Total: $" + cobro)
+  }
 
+  else if (cantidad > 24999) {
 
-  alert(cantidad1);
+    let cobro = cantidad * .20;
+    let aseguramiento = 2000
+    let cobroCliente = cobro + aseguramiento
+
+    alert("Asesoría: $" + cobro + "\n" + "Aseguramiento: $" + aseguramiento + "\n" + "Total: $ " + cobroCliente)
+  }
+
+  else if (cantidad > 14999) {
+    let cobro = cantidad * .20;
+    let aseguramiento = 1700
+    let cobroCliente = cobro + aseguramiento;
+
+    alert("Asesoría: $" + cobro + "\n" + "Aseguramiento: $" + aseguramiento + "\n" + "Total: $" + cobroCliente)
+  }
+
+  else {
+    let cobro = cantidad * .20
+    let aseguramiento = 1300
+    let cobroCliente = cobro + aseguramiento
+
+    alert("Asesoría: $" + cobro + "\n" + "Aseguramiento: $" + aseguramiento + "\n" + "Total: $" + cobroCliente)
+  }
+
 }
 
 
+//function focus & blur
 
+document.querySelector('input').addEventListener('focus', () => {
+  console.log('focus');
+  document.querySelector('.label').classList.toggle('active')
+
+})
+
+document.querySelector('input').addEventListener('blur', () => {
+  console.log('Blur')
+  document.querySelector('.label').classList.toggle('active')
+
+})
